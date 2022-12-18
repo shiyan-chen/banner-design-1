c1a9bb593df536c55059c398f1eb6fb92153ea6c
Shiyan Chen <ch592127418@gmail.com>
2022-12-17 19:49:58 -0800

    Updates

---
+++ precache-manifest.8d9d9c0b773fc635f50b1eb032dfbca5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c091215db38dfbc4ca6f",
+    "url": "/static/css/main.562e3001.chunk.css"
+  },
+  {
+    "revision": "c091215db38dfbc4ca6f",
+    "url": "/static/js/main.525da30a.chunk.js"
+  },
+  {
+    "revision": "99798f2f2cd34153bd78",
+    "url": "/static/js/runtime~main.1ae3213d.js"
+  },
+  {
+    "revision": "c58cb75a7feaf5003ac1",
+    "url": "/static/js/2.e6637c1e.chunk.js"
+  },
+  {
+    "revision": "cd9dcf72c3151b386634",
+    "url": "/static/js/3.3331c834.chunk.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "fb4e771e1a4c27ae5c2a05a3b3124987",
+    "url": "/index.html"
+  }
+];
